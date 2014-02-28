e564a108e9416af3def07751f131f2c242dfd664
macournoyer <macournoyer@gmail.com>
2014-02-27 22:16:09 -0500

    Remove files not needed.

--- test/compile_test.js
+++ test/compile_test.js
@@ -3,9 +3,4 @@ var assert = require("assert"),
 
 describe('sss.toCSS', function() {
   
-  
-  function compile(input) {
-    // Squeeze the whitespaces to make sure tests don't break if we modify indentation or line breaks.
-    return sss.toCSS(input).replace(/\n+/g, ' ').replace(/ +/g, ' ')
-  }
 })
